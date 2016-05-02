f5c9e75e8b291ba117c43727f456f7a5d02c29e2
sam-mckay <sammckay@btinternet.com>
2016-05-02 13:32:12 +0100

    Basic Project Setup
    Folders
    Scripts: Tags, SaveManager

---
+++ IndonesiaGame/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Use for any game data that needs storing, before/during/after game/scene switching
+/// </summary>
+public class SaveManager
+{
+    //add items in below format
+    //public static string  = "";
+
+}
---
+++ IndonesiaGame/Assets/Scripts/Tags.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// For Adding new tags for game objects
+/// </summary>
+public class Tags
+{
+    public static string player = "Player";
+	
+}
